172348eb9fe7d633b4842c2328a431c3c7725954
기대현 <dh@gidaehyeon-ui-MacBookPro-2.local>
2022-08-17 18:01:24 +0900

    Add: University Search

--- univ_interests/utils/auth.py
+++ univ_interests/utils/auth.py
@@ -8,7 +8,8 @@ from rest_framework.status import HTTP_401_UNAUTHORIZED
 
 def login_required(func):
     @wraps(func)
-    def decorated(self, request):
+    def decorated(self, *args, **kwargs):
+        request = self.request
         access_token = request.headers.get('Authorization', None)
         if access_token is None:
             return Response({'detail': 'Invalid Token'}, status=HTTP_401_UNAUTHORIZED)
@@ -17,5 +18,5 @@ def login_required(func):
                 payload = jwt.decode(access_token, 'SECRET', 'HS256')
             except jwt.InvalidTokenError:
                 return Response({'detail': 'Invalid Token'}, status=HTTP_401_UNAUTHORIZED)
-        return func(self, request, user=payload)
+        return func(self, *args, **kwargs, user=payload)
     return decorated
