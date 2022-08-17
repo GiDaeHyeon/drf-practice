4b71d6f7cac86de285fdf88a5574840cdf56b7a8
기대현 <dh@gidaehyeon-ui-MacBookPro-2.local>
2022-08-17 13:36:45 +0900

    Add: Sign-In

--- univ_interests/views.py
+++ univ_interests/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from .serializers import SignUpSerializer
+from rest_framework.status import HTTP_400_BAD_REQUEST, HTTP_200_OK
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class SignUpView(APIView):
+    def post(self, request):
+        serializer = SignUpSerializer(data=request.data)
+
+        if serializer.is_valid():
+            user = serializer.create(validated_data=serializer.validated_data)
+            return Response({'detail': 'success', 'user_data': user}, status=HTTP_200_OK)
+        else:
+            return Response(serializer.errors, status=HTTP_400_BAD_REQUEST)
